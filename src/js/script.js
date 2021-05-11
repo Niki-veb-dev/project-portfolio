5f6dc2ea2ac68042660788c4f7422ee4f79b7eba
Nikolas <tes.kolia@gmail.com>
2021-05-11 22:29:26 +0300

    first commit portfolio

---
+++ src/js/script.js
@@ -0,0 +1,51 @@
+const hamburger = document.querySelector('.hamburger'),
+        menu = document.querySelector('.menu'),
+        closeElem = document.querySelector('.menu__close');
+
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+const ratingCounter = document.querySelectorAll('.rating__counter'),
+        lines = document.querySelectorAll('.rating__line span');
+
+ratingCounter.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML; 
+});
+
+
+// var number = 5;
+// var string = "Hello";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
+
+// console.log();
+// let person = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// }
+// // console.log(person[]);
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+// // console.log(arr[2]);
+// // // alert("Hello bitch");
+// let answer = +prompt("Are u here? do you have 18 year");
+// console.log(typeof(answer));
+'use strict';
+
+let incr =10,
+    decr =10;
+    console.log(incr++);
+    console.log(decr--);
+    console.log(5%2);
+let isChecked = true,
+    isClose = false;
+    console.log(isChecked || !isClose); 
